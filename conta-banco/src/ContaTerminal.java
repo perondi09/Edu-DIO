1d60497640f91a5b5a7ce20727c5f9f14406be1d
perondi <80803152+perondi09@users.noreply.github.com>
2024-11-18 15:05:33 -0300

    Subindo desafio conta bancaria, aula sintaxe com Java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.println("Por favor, digite o número da Agência:");
+            String agencia = scanner.nextLine();
+
+            System.out.println("Por favor, digite o número da Conta:");
+            int numero = scanner.nextInt();
+
+            scanner.nextLine();
+
+            System.out.println("Por favor, digite o Nome do Cliente:");
+            String nomeCliente = scanner.nextLine();
+
+            System.out.println("Por favor, digite o Saldo inicial:");
+            double saldo = scanner.nextDouble();
+
+            String mensagem = String.format(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.",
+                nomeCliente, agencia, numero, saldo
+            );
+
+            System.out.println(mensagem);
+        }
     }
 }
